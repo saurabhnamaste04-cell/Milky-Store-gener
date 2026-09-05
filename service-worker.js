60cbb03ace84f6f5efd78be2af723266835f0ac4
saurabhnamaste04-cell <saurabhnamaste04@gmail.com>
2026-09-05 22:20:35 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE='milky-store-v2';
+const ASSETS=['./','./index.html','./qr_only.png','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
